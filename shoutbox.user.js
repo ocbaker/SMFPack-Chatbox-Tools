96a33540a582b077cad20c33d2aa5dbc07ef5afa
Oliver C. Baker <oliver.baker@sixnetworks.net>
2015-05-21 18:26:15 +0200

    Update shoutbox.user.js

--- shoutbox.user.js
+++ shoutbox.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name       Chatbox Notifications
 // @namespace  https://github.com/ocbaker/SMFPack-Chatbox-Tools/
-// @version    0.1
-// @description  enter something useful
+// @version    0.2
+// @description  Adds some great functionality for the SMF Chatbox (Designed with LegendsOfEquestria in mind)
 // @match      http://forum.legendsofequestria.com/index.php?action=chatpage
 // @match      http://forum.legendsofequestria.com/index.php?action=shoutbox
-// @copyright  2012+, You
+// @updateURL  https://github.com/ocbaker/SMFPack-Chatbox-Tools/raw/master/shoutbox.meta.js
+// @downloadURL  https://github.com/ocbaker/SMFPack-Chatbox-Tools/raw/master/shoutbox.user.js
+// @copyright  2012+, Oliver Baker
 // @run-at document-end
 // ==/UserScript==
 
